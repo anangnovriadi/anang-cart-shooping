60c9c74bb85c6714224358cfde6314485ff23b11
Anang Novriadi <anangnov99@gmail.com>
2024-02-12 18:12:48 +0700

    fix cart list

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -6,12 +6,12 @@
         <div class="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             @foreach ($products as $product)
             <div class="w-full max-w-sm mx-auto overflow-hidden rounded-md shadow-md">
-                <img src="{{ url($product->image) }}" alt="" class="w-full max-h-60">
+                <img src="{{ url($product->image) }}" alt="" class="w-full h-60">
                 <div class="flex items-end justify-end w-full bg-cover">
                 </div>
-                <div class="px-5 py-3">
-                    <h3 class="text-gray-700 uppercase">{{ $product->name }}</h3>
-                    <span class="mt-2 text-gray-500">${{ $product->price }}</span>
+                <div class="p-3">
+                    <h4 class="text-gray-700">{{ $product->name }}</h4>
+                    <span class="mt-2 text-gray-500">@currency($product->price)</span>
                 </div>
             </div>
             @endforeach
